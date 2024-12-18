016a4b78e99de9919fee1bbd28f6671ac98c07d3
dfacvzc <shaw2480@hanmail.net>
2024-12-18 11:02:35 +0900

    Add files via upload

---
+++ intro.php
@@ -0,0 +1,8 @@
+<?php 
+include "head.sub.php";?>
+ 
+<div class="intro">
+	
+</div>
+<?php include "tail.sub.php";?>
+ 
---
+++ intro2.php
@@ -0,0 +1,8 @@
+<?php 
+include "head.sub.php";?>
+ 
+<div class="intro2">
+	
+</div>
+<?php include "tail.sub.php";?>
+ 
